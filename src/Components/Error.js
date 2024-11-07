8540762d09b0eca0bed87707d1371385f3834b9a
Supanusan <ssupanusan@gmail.com>
2024-11-07 08:55:38 +0530

    update

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -6,7 +6,7 @@ const Error = () => {
         <div className='flex flex-col justify-center items-center h-screen w-full text-9xl sm:text-center '>
             <FaRegSadTear />
             <h1 className='text-yellow-400'>Your are not verified user !</h1>
-            <h1>Please Contact <a href="https://supanusan.netlify.app/">Admin</a></h1>
+            <h1>Please Contact <a href="https://supanusan.netlify.app/"><span className='underline'>Admin</span></a></h1>
         </div>
     )
 }
